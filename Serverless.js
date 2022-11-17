6b681be794c6ef1e5fbd5e03cb0c93278e6e7612
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2022-11-17 12:24:58 -0500

    adding the depencies

---
+++ Serverless.js
@@ -0,0 +1,14 @@
+var aws = require("aws-sdk");
+aws.config.update({region:'us-east-1'})
+var ses = new aws.SES({ region: "us-east-1" });
+var docClient = new aws.DynamoDB.DocumentClient();
+
+exports.handler = async function (event) {
+    let message = event.Records[0].Sns.Message
+    let json = JSON.parse(message);
+    let email = json.EmailId;
+    let token = json.token;
+    const seconds = 5 * 60;
+    const secondsInEpoch = Math.round(Date.now() / 1000);
+    const expirationTime = secondsInEpoch + seconds;
+    const currentTime = Math.round(Date.now() / 1000);
